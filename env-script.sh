f209bbf5694a7cc1d43450d45d8ee7b06a5aafa8
Tobias Boner <dydent.github@outlook.com>
2022-04-08 19:00:32 +0200

    update start echo

--- env-script.sh
+++ env-script.sh
@@ -7,7 +7,7 @@ printenv | sed 's/^\(.*\)$/\1/g' > /tmp/env.sh
 # check where the date command is located to avoid command not found error
 date_command_location=$(command -v date)
 cronStartTime=$(${date_command_location} +%m-%d-%Y_%H:%M:%S)
-echo "start running cron at ${cronStartTime}:"
+echo "start running backup cron at ${cronStartTime}:"
 # run cron with output
 crond -f -l 2
 
